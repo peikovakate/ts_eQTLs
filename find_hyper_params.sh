dbf6bd0817662d73915a712f5423f3a6867adade
peikovakate <peikovakate@gmail.com>
2020-09-14 17:34:28 +0300

    parameters update

--- find_hyper_params.sh
+++ find_hyper_params.sh
@@ -5,11 +5,11 @@ xf="../data/gtex/mfactorization/slope.txt"
 wf="../data/gtex/mfactorization/se.txt"
 od="../data/gtex/mfactorization/"
 
-for K in 10 15 20
+for K in 20 30 40
 do
-        for alpha in 10 100 500 1000 1500
+        for alpha in 2000 3000 4000
         do
-                for lambda in 10 100 500 1000 1500
+                for lambda in 2000 3000 4000
                 do
                         sbatch sn_spMF/1_run_parameter_scope_search.sh ${K} ${alpha} ${lambda} ${iterations} ${xf} ${wf} ${od}
                 done
