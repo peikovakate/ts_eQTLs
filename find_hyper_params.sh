07c9b98e09557b0301b66f7341c46a1c601e927a
peikovakate <peikovakate@gmail.com>
2020-09-14 17:18:46 +0300

    settings

---
+++ find_hyper_params.sh
@@ -0,0 +1,17 @@
+rm slurm-*
+
+iterations=20
+xf="../data/gtex/mfactorization/slope.txt"
+wf="../data/gtex/mfactorization/se.txt"
+od="../data/gtex/mfactorization/"
+
+for K in 10 15 20
+do
+        for alpha in 10 100 500 1000 1500
+        do
+                for lambda in 10 100 500 1000 1500
+                do
+                        bash sn_spMF/1_run_parameter_scope_search.sh ${K} ${alpha} ${lambda} ${iterations}
+                done
+        done
+done
--- sn_spMF/1_run_parameter_scope_search.sh
+++ sn_spMF/1_run_parameter_scope_search.sh
@@ -2,7 +2,9 @@
 #SBATCH --time 12:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH -p skylake
+#SBATCH -p main
+#SBATCH -t 12:00:00
+#SBATCH 
 
 
 ### Search the appropriate range for alpha1 and lambda1
@@ -12,12 +14,14 @@
 K="$1"
 alpha1="$2"
 lambda1="$3"
-
 iterations="$4"
 
+xf="$5"
+wf="$6"
+od="$7"
 
-ml R/3.5.1
-Rscript sn_spMF/run_MF.R -k $K -a ${alpha1} -l ${lambda1} -t ${iterations}
+module load R/3.5.1
+Rscript sn_spMF/run_MF.R -k $K -a ${alpha1} -l ${lambda1} -t ${iterations} -x ${xf} -w ${wf} -O ${od}
 
 
 ### Examine the printed output to narrow down the proper range of alpha1 and lambda1 for further search
