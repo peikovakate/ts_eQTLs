e0612d6e44950446bdbaff6cd116d5d0e6734956
peikovakate <peikovakate@gmail.com>
2020-09-29 11:43:01 +0300

    fix directory path

--- map.sh
+++ map.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --time 24:00:00
+#SBATCH --time 30:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH -p main
@@ -12,6 +12,6 @@ M="$1"
 xf="../data/gtex/mfactorization/slope.txt"
 wf="../data/gtex/mfactorization/se.txt"
 od="../data/gtex/mfactorization/"
-mod="../data/gtex/mfactorizaion/mapping/"
+mod="../data/gtex/mfactorization/mapping/"
 
 Rscript mapping/lm.R -f $M -x ${xf} -w ${wf} -d ${od} -m ${mod}
