8b413ab79703f26a30208f739da600ae6196cc0f
peikovakate <peikovakate@gmail.com>
2020-10-06 13:03:13 +0300

    parallel matrix factorization

--- sn_spMF/2_choose_hyperparameters.sh
+++ sn_spMF/2_choose_hyperparameters.sh
@@ -3,7 +3,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH -p main
-
+#SBATCH --array=1-20
 
 K="$1"
 alpha1="$2"
@@ -14,5 +14,7 @@ xf="$5"
 wf="$6"
 od="$7"
 
+runind="$SLURM_ARRAY_TASK_ID"
+
 module load R/3.5.1
-Rscript sn_spMF/run_MF.R -k $K -a ${alpha1} -l ${lambda1} -t ${iterations} -c 1 -x ${xf} -w ${wf} -O ${od}
+Rscript sn_spMF/run_MF.R -k $K -a ${alpha1} -l ${lambda1} -t ${iterations} -c 1 -x ${xf} -w ${wf} -O ${od} -r ${runind}
