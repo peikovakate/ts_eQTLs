3ee7e1405d69427439d2159b9a7316ec540458ea
peikovakate <peikovakate@gmail.com>
2020-10-11 13:45:38 +0300

    update mapping directory

--- map.sh
+++ map.sh
@@ -11,7 +11,7 @@ M="$1"
 
 xf="../data/gtex/mfactorization/slope.txt"
 wf="../data/gtex/mfactorization/se.txt"
-od="../data/gtex/mfactorization/"
-mod="../data/gtex/mfactorization/mapping/"
+od="../data/gtex/mfactorization2/"
+mod="../data/gtex/mfactorization2/mapping/"
 
 Rscript mapping/lm.R -f $M -x ${xf} -w ${wf} -d ${od} -m ${mod}
